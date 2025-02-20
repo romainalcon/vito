1dd34c70da271c844e2a990413238d2afe024ebe
Romain Alcon <41171922+romainalcon@users.noreply.github.com>
2025-02-20 14:02:50 +0100

    fix: use any instead of ipv4

--- resources/views/ssh/services/firewall/ufw/install-ufw.blade.php
+++ resources/views/ssh/services/firewall/ufw/install-ufw.blade.php
@@ -6,15 +6,15 @@ if ! sudo ufw default allow outgoing; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
-if ! sudo ufw allow from 0.0.0.0/0 to any proto tcp port 22; then
+if ! sudo ufw allow from any to any proto tcp port 22; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
-if ! sudo ufw allow from 0.0.0.0/0 to any proto tcp port 80; then
+if ! sudo ufw allow from any to any proto tcp port 80; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
-if ! sudo ufw allow from 0.0.0.0/0 to any proto tcp port 443; then
+if ! sudo ufw allow from any to any proto tcp port 443; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
